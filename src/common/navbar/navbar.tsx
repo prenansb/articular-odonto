a434edd3f02a41657388614a458374ddfc1b8ee7
Pedro Renan <prenansb@gmail.com>
2022-04-19 01:01:43 -0300

    responsive navbar

--- src/common/navbar/navbar.tsx
+++ src/common/navbar/navbar.tsx
@@ -1,11 +1,35 @@
-import { LogoSvg, MenuSvg } from './svgs'
+import { LogoSvg, MenuSvg, WhatsappSvg } from './svgs'
+
+const links = [
+  { name: 'Qualidades', path: '' },
+  { name: 'Tratamentos', path: '' },
+  { name: 'Sobre nós', path: '' },
+  { name: 'Depoimentos', path: '' },
+  { name: 'Localização', path: '' },
+]
 
 export default function Navbar() {
   return (
-    <header className="flex items-center h-[90px] px-[24px] pt-[18px]">
-      <nav className="flex justify-between items-center w-full">
+    <header className="flex items-center h-[90px] px-[24px] pt-[18px] md:px-[48px] xl:px-0 xl:max-w-[1216px] xl:mx-auto">
+      <nav className="flex justify-between items-center xl:justify-between  w-full">
         <LogoSvg />
-        <MenuSvg />
+        <ul className="hidden lg:flex items-center gap-[40px]">
+          {links.map(link => (
+            <li
+              className="text-[#8F9CA9] text-[14px] leading-[24px] font-normal"
+              key={link.name}
+            >
+              {link.name}
+            </li>
+          ))}
+        </ul>
+        <button className="hidden xl:flex items-center justify-center gap-[20px] bg-[#F6F6F8] border border-[#54B6AE] rounded-[4px] py-[12px] px-[24px]">
+          <WhatsappSvg />
+          <a href="#" className="text-[14px] text-[#54B6AE] font-medium leading-[24px]">
+            Agende sua consulta
+          </a>
+        </button>
+        <MenuSvg className="lg:hidden" />
       </nav>
     </header>
   )
--- src/common/navbar/svgs/index.ts
+++ src/common/navbar/svgs/index.ts
@@ -1,2 +1,4 @@
 export { default as LogoSvg } from './logo'
 export { default as MenuSvg } from './menu'
+export { default as WhatsappSvg } from './whatsapp'
+
