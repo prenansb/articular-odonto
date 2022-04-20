d91e4255cf5afef61cc5cc2ce6c4e2bdda0f190d
Pedro Renan <prenansb@gmail.com>
2022-04-20 00:11:06 -0300

    responsive footer

--- src/common/footer/copyrights.tsx
+++ src/common/footer/copyrights.tsx
@@ -3,13 +3,12 @@ import { GhostSvg } from './svgs'
 export default function Copyrights() {
   return (
     <>
-      <div className="w-full h-[1px] bg-[#233B37]" />
-      <div className="flex-col mt-[40px]">
-        <div className="font-semibold text-[18px] leading-[30px] text-[#233B37] text-center">
+      <div className="flex flex-col mt-[40px] xl:flex-row xl:items-center xl:justify-between xl:mt-[48px] xl:mb-[56px]">
+        <div className="font-semibold text-[18px] md:text-[20px] leading-[30px] text-[#233B37] text-center">
           Articular Odonto.
-          <br /> Todos os direitos reservados
+          <br className="md:hidden" /> Todos os direitos reservados
         </div>
-        <div className="flex items-center justify-center gap-[16px] my-[32px] text-[#233B37] text-[18px] leading-[30px] font-semibold">
+        <div className="flex items-center justify-center gap-[16px] my-[32px] text-[#233B37] text-[18px] md:text-[20px] leading-[30px] font-semibold">
           Desenvolvido por: <GhostSvg />
         </div>
       </div>
--- src/common/footer/informations.tsx
+++ src/common/footer/informations.tsx
@@ -1,34 +1,57 @@
-import { LogoSvg } from './svgs'
+import { InstagramSvg, LogoSvg, WhatsappSvg } from './svgs'
+
+const socials = [
+  { name: 'Whatsapp', icon: <WhatsappSvg /> },
+  { name: 'Instagram', icon: <InstagramSvg /> },
+]
 
 export default function Informations() {
   return (
-    <>
-      <div className="w-full h-[1px] bg-[#233B37]" />
-
-      <div className="flex flex-col items-start gap-[32px] mt-[32px] mb-[48px]">
-        <LogoSvg />
-        <span className="max-w-[147px] font-medium text-[18px] leading-[27px] text-[#667F7B]">
-          Uma vida melhor, começa com um belo sorriso!
-        </span>
+    <div className="md:flex md:justify-between md:mt-[48px] xl:mb-[96px]">
+      <div className="xl:">
+        <div className="flex flex-col items-start gap-[32px] mt-[32px] mb-[48px] md:mt-0">
+          <LogoSvg />
+          <span className="max-w-[150px] font-medium text-[18px] leading-[27px] text-[#667F7B]">
+            Uma vida melhor, começa com um belo sorriso!
+          </span>
+        </div>
       </div>
 
-      <div>
-        <h4 className="mb-[8px] font-medium text-[20px] leading-[27px] text-[#36414C]">
-          Endereço da clínica
-        </h4>
-        <span className="font-medium text-[16px] leading-[24px] text-[#667F7B]">
-          Avenida Humberto Monte - 1440, Fortaleza, Ceará.
-        </span>
-      </div>
+      <div className="xl:flex xl:gap-[200px]">
+        <div>
+          <span className="hidden md:block md:mb-[24px] text-[24px] text-[#233B37] leading-[27px] font-semibold">
+            Informações para contato
+          </span>
+          <div className="md:mb-[32px]">
+            <h4 className="mb-[8px] font-medium text-[20px] leading-[27px] text-[#36414C]">
+              Endereço da clínica
+            </h4>
+            <span className="md:max-w-[320px] font-medium text-[16px] leading-[24px] text-[#667F7B] ">
+              Avenida Humberto Monte - 1440, Fortaleza, Ceará.
+            </span>
+          </div>
 
-      <div className="mt-[32px] mb-[56px]">
-        <h4 className="mb-[8px] font-medium text-[20px] leading-[27px] text-[#36414C]">
-          Telefone para contato
-        </h4>
-        <span className="font-medium text-[16px] leading-[24px] text-[#667F7B]">
-          (85) 9.9999-9999 ou (85) 9.9999-9999{' '}
-        </span>
+          <div className="mt-[32px] mb-[56px]">
+            <h4 className="mb-[8px] font-medium text-[20px] leading-[27px] text-[#36414C]">
+              Telefone para contato
+            </h4>
+            <span className="font-medium text-[16px] leading-[24px] text-[#667F7B]">
+              (85) 9.9999-9999 ou (85) 9.9999-9999{' '}
+            </span>
+          </div>
+        </div>
+
+        <div className="mb-[48px] xl:">
+          <span className="mb-[16px] font-semibold text-[24px] leading-[27px] text-[#233B37]">
+            Conecte-se conosco:
+          </span>
+          <ul className="flex items-center gap-[24px] mt-[24px]">
+            {socials.map(social => (
+              <li key={social.name}>{social.icon}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
