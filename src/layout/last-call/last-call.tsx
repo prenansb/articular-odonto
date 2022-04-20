f7c7113952c6fecf5d9b0d56a8a924b23b9a2e6d
Pedro Renan <prenansb@gmail.com>
2022-04-20 00:10:47 -0300

    responsive last-call

--- src/layout/last-call/last-call.tsx
+++ src/layout/last-call/last-call.tsx
@@ -9,10 +9,10 @@ export default function LastCall() {
         <Image src={BannerImg} alt="" unoptimized />
       </div>
       <div className="flex flex-col items-center z-10">
-        <h2 className="max-w-[242px] text-[24px] text-[#F7F7F7] font-bold leading-[36px] text-center mt-[48px] mb-[16px]">
+        <h2 className="max-w-[242px] text-[24px] text-[#F7F7F7] font-bold leading-[36px] text-center mt-[48px] mb-[16px] md:text-[40px] md:leading-[60px] md:max-w-[403px]">
           Estamos juntos para fazer você sorrir!
         </h2>
-        <p className="max-w-[267px] text-[16px] text-[#F6F6F8] font-medium leading-[24px] text-center mb-[32px]">
+        <p className="max-w-[267px] text-[16px] text-[#F6F6F8] font-medium leading-[24px] text-center mb-[32px] md:text-[18px] md:leading-[27px] md:max-w-[301px]">
           Agende sua consulta conosco, temos o maior prazer em te receber
         </p>
         <button className="flex items-center justify-center gap-[20px] bg-[#F6F6F8] rounded-[4px] py-[14px] px-[32px] mb-[56px]">
