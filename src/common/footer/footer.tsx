905e2089119e30300b0661b6e90f9310cf9bdb91
Pedro Renan <prenansb@gmail.com>
2022-04-21 21:30:42 -0300

    footer divided changed

--- src/common/footer/footer.tsx
+++ src/common/footer/footer.tsx
@@ -2,12 +2,15 @@ import { Copyrights, Informations } from '.'
 
 export default function Footer() {
   return (
-    <footer className="mt-[80px] px-[24px] xl:max-w-[1216px] xl:mx-auto">
+    <footer className="mt-[80px]">
       <div className="w-full h-[1px] bg-[#233B37]" />
-      <Informations />
 
-      <div className="w-full h-[1px] bg-[#233B37]" />
-      <Copyrights />
+      <div className="px-[24px] xl:max-w-[1216px] xl:mx-auto">
+        <Informations />
+
+        <div className="w-full h-[1px] bg-[#233B37]" />
+        <Copyrights />
+      </div>
     </footer>
   )
 }
