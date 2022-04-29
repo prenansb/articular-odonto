31930af1237f307c760474041acec3b2a741f371
Raniere Marques <Raniere852@outlook.com>
2022-04-29 18:28:46 -0300

    font optimization and self hosted

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import { Footer, Navbar } from '../common'
@@ -7,6 +8,18 @@ import '../styles/global-style.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/mulish-var-latin.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,15 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
