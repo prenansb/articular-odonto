01e99c5b13afba1b362191392f7158876eccea6e
Raniere Marques <Raniere852@outlook.com>
2022-05-12 07:14:34 -0300

    twitter card test 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,25 +23,7 @@ const Index: NextPage = () => {
         <meta property="og:title" content="Articular Odonto" />
         <meta property="og:description" content="Uma descrição aqui" />
         <meta property="og:image" content="/imgs/banner-share.png" />
-        <meta property="og:site_name" content="Articular Odonto" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Articular Odonto" />
-        <meta name="twitter:description" content="Uma descrição aqui" />
-        <meta
-          name="twitter:image"
-          content="https://articular-odonto.vercel.app/imgs/banner-share.png"
-        />
-        <meta
-          name="twitter:image:src"
-          content="https://articular-odonto.vercel.app/imgs/banner-share.png"
-        />
-        <meta name="twitter:site" content="@rocketseat" />
-        <meta name="twitter:creator" content="@rocketseat" />
-        <meta name="twitter:image:alt" content="Boost yourself" />
-        <meta name="twitter:image:width" content="1200" />
-        <meta name="twitter:image:height" content="630" />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="pt_BR" />
       </Head>
       <Home />
       <Qualities />
