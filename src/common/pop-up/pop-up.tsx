95f18d4d7707901b766eef3def574535c030a3bb
Raniere Marques <Raniere852@outlook.com>
2022-05-14 06:37:31 -0300

    refactoring popup 1/2

--- src/common/pop-up/pop-up.tsx
+++ src/common/pop-up/pop-up.tsx
@@ -1,68 +1,49 @@
-import { useEffect, useState } from 'react'
-import Sheet from 'react-modal-sheet'
+import { useEffect, useRef } from 'react'
 import { WhatsappSvg } from './svgs'
 
 export default function PopUp() {
-  const [isOpen, setOpen] = useState(false)
-  const [snapPoints, setSnapPoints] = useState<number[]>([])
+  const popUpRef = useRef<HTMLDivElement>(null)
+
+  function showPopUpOnScroll() {
+    const ref = popUpRef.current
+
+    if (!ref) return
+
+    const heightInPixelsToShowPopUp = 460
+
+    window.scrollY >= heightInPixelsToShowPopUp
+      ? ref.classList.add('showPopUp')
+      : ref.classList.remove('showPopUp')
+  }
 
   useEffect(() => {
-    if (screen.width <= 425) {
-      setSnapPoints([110, 0])
-      setTimeout(() => {
-        setOpen(true)
-      }, 10000)
-      if (screen.width === 320) {
-        setSnapPoints([100, 0])
-      }
+    window.addEventListener('scroll', showPopUpOnScroll)
+
+    return () => {
+      window.removeEventListener('scroll', showPopUpOnScroll)
     }
   }, [])
 
   return (
-    <>
-      <Sheet isOpen={isOpen} onClose={() => setOpen(false)} snapPoints={snapPoints}>
-        <Sheet.Container
-          onViewportBoxUpdate={() => {
-            /* TODO document why this arrow function is empty */
-          }}
-        >
-          <Sheet.Header
-            onViewportBoxUpdate={() => {
-              /* TODO document why this arrow function is empty */
-            }}
-          />
-          <Sheet.Content
-            onViewportBoxUpdate={() => {
-              /* TODO document why this arrow function is empty */
-            }}
-          >
-            <div className="w-full px-6 flex items-center gap-8 xsm:gap-[34px] justify-center">
-              <div>
-                <h6 className="text-sm text-[#36414C] leading-[18px] font-semibold">
-                  Dúvidas?
-                </h6>
-                <span className="text-xs text-[#66737F] leading-[18px] font-medium">
-                  Fale conosco
-                </span>
-              </div>
-              <a
-                href="#"
-                draggable="false"
-                className="flex items-center justify-center gap-[16px] xsm:py-[10px] xsm:px-[14px] px-[23px] py-[16px] xsm:text-[10px] bg-[#54B6AE] hover:brightness-95 active:brightness-90 transition text-[14px] text-[#FFFFFF] font-medium leading-[24px] rounded-[4px]"
-              >
-                <WhatsappSvg />
-                Chamar no whatsapp
-              </a>
-            </div>
-          </Sheet.Content>
-        </Sheet.Container>
-
-        <Sheet.Backdrop
-          onViewportBoxUpdate={() => {
-            /* TODO document why this arrow function is empty */
-          }}
-        />
-      </Sheet>
-    </>
+    <div
+      ref={popUpRef}
+      className="sm:invisible fixed bg-[#EAEFF4] transition shadow-[0_-2px_12px_#0000001e] pb-4 pt-6 rounded-t-2xl bottom-0 z-10 w-full px-6 invisible flex items-center gap-8 justify-center"
+    >
+      <div className="absolute top-2.5 w-10 h-1 bg-[#D5DDE5] rounded-full" />
+      <div className="flex flex-col gap-2">
+        <h6 className="text-sm text-[#36414C] leading-[18px] font-semibold">Dúvidas?</h6>
+        <span className="text-xs text-[#66737F] leading-[18px] font-medium">
+          Fale conosco
+        </span>
+      </div>
+      <a
+        href="#"
+        draggable="false"
+        className="flex items-center justify-center gap-[16px] px-[23px] py-[16px] bg-[#54B6AE] hover:brightness-95 active:brightness-90 transition text-[14px] text-[#FFFFFF] font-medium leading-[24px] rounded-[4px]"
+      >
+        <WhatsappSvg />
+        Chamar no whatsapp
+      </a>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    screens: {
-      xsm: { max: '320px' },
-    },
     extend: {},
   },
   plugins: [],
