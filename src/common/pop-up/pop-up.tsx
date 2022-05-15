72257099b1243f6cd3823473a15eba93f5b34b29
Raniere Marques <Raniere852@outlook.com>
2022-05-15 09:48:51 -0300

    popup refactored

--- src/common/pop-up/pop-up.tsx
+++ src/common/pop-up/pop-up.tsx
@@ -33,38 +33,33 @@ export default function PopUp() {
 
   function handlePopUpStart(event: TouchEvent) {
     translateRef.current.start = getHeightDislocation(event)
-
     if (!popUpRef.current) return
-
     popUpRef.current.style.transitionDuration = '0ms'
   }
 
   function handlePopUpMove(event: TouchEvent) {
-    const heightDislocation = getHeightDislocation(event)
-    const translateHeight = translateRef.current.start - heightDislocation
-    const translateActual = translateRef.current.end + translateHeight
-
-    const transformHeight = () => {
-      if (translateActual < 0) return 0
-      if (translateActual > 94) return 94
-      return translateActual
-    }
-
-    translateRef.current.actual = transformHeight()
-
     if (!popUpRef.current) return
 
-    popUpRef.current.style.transform = `translateY(${translateRef.current.actual}px)` // 0px to 94px
+    const popUpHeight = popUpRef.current.offsetHeight
+    const heightDislocation = getHeightDislocation(event)
+    const translateHeight = translateRef.current.start - heightDislocation
+    let translateActual = translateRef.current.end + translateHeight
+
+    if (translateActual < 0) translateActual = 0
+    if (translateActual > popUpHeight) translateActual = popUpHeight
+
+    translateRef.current.actual = translateActual
+    popUpRef.current.style.transform = `translateY(${translateRef.current.actual}px)`
   }
 
   function handlePopUpEnd() {
-    const containerHeightHalf = 94 / 2
-    const newTransform = translateRef.current.actual < containerHeightHalf ? 0 : 94
-
-    translateRef.current.end = newTransform
-
     if (!popUpRef.current) return
 
+    const popUpHeight = popUpRef.current.offsetHeight
+    const breakpoint = popUpHeight / 2
+    const translate = translateRef.current.actual < breakpoint ? 0 : popUpHeight
+
+    translateRef.current.end = translate
     popUpRef.current.style.transform = `translateY(${translateRef.current.end}px)`
     popUpRef.current.style.transitionDuration = '200ms'
   }
@@ -72,7 +67,7 @@ export default function PopUp() {
   return (
     <div
       ref={popUpRef}
-      className="sm:invisible touch-none fixed will-change-transform bg-[#EAEFF4] transition ease-out shadow-[0_-2px_12px_#0000001e] pb-4 pt-6 rounded-t-2xl bottom-0 z-10 w-full px-6 invisible flex items-center gap-8 justify-center"
+      className="sm:invisible fixed touch-none will-change-transform bg-[#EAEFF4] transition ease-out shadow-[0_-2px_12px_#0000001e] pb-4 pt-6 rounded-t-2xl bottom-0 z-10 w-full px-6 invisible flex items-center gap-8 justify-center"
     >
       <div
         onTouchStart={handlePopUpStart}
